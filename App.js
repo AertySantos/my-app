74db964b985917ce269246a6ea0776c6471c7968
aertyps <aertyps@hotmail.com>
2023-08-21 18:11:59 -0300

    initial

--- App.js
+++ App.js
@@ -1,20 +1,54 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import React, { Component }from 'react';
+import{
+  View,
+  Text,
+  StyleSheet,
+  TouchableOpacity,
+  Image } from 'react-native';
 
-export default function App() {
-  return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
-  );
+class App extends Component{
+  render(){
+    return(
+      <View style={styles.container}>
+        <Image 
+        source = {require('./src/biscoito.png')}
+        style = {styles.img}
+        />
+        <Text style={styles.textoFrase}>" Alguma frase aqui "</Text>
+
+        <TouchableOpacity style={styles.botao}> 
+          <View style={styles.btnArea}>
+            <Text style={styles.btnTexto}>
+              Quebrar Biscoito
+              </Text>
+            </View>
+        </TouchableOpacity>
+      </View>  
+    )
+  };
 }
 
 const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+conteiner:{
+  flex:1,
+  
+  },
+  img:{
+    width:250,
+    height:250,
+  },
+  textoFrase: {
+    
+  },
+  botao:{
+
+  },
+  btnArea:{
+  
+  },
+  btnTexto:{
+
   },
 });
+
+export default App;
